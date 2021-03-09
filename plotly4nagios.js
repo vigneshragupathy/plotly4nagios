565006319b8435605601aa33e004025373b85bf5
Vignesh Ragupathy <me@vikki.in>
2021-03-09 18:36:19 +0530

    Simple working format created

--- plotly4nagios.js
+++ plotly4nagios.js
@@ -1,33 +1,40 @@
-Plotly.d3.csv("https://raw.githubusercontent.com/vignesh88/plotly4nagios/main/nagios.csv", function(err, rows){
+Plotly.d3.dsv(';')("https://raw.githubusercontent.com/vignesh88/plotly4nagios/main/nagios.csv", function (err, rows) {
 
   function unpack(rows, key) {
-  return rows.map(function(row) { return row[key]; });
-}
+    return rows.map(function (row) {
+      return row[key];
+    });
+  }
+
+  var columns = Object.keys(rows[1]);
+  var data_final = [];
+
+  for (var i = 1; i < columns.length - 3; i++) {
+    if (columns[i].includes("_AVERAGE")) {
+      console.log(columns[i]);
+      var trace_all = {
+        type: "scatter",
+        mode: "lines+markers",
+        name: columns[i].replace("_AVERAGE", ""),
+        x: unpack(rows, 'timestamp').map(parseTime),
+        y: unpack(rows, columns[i])
+        //line: {color: '#17BECF'}
+      };
+      data_final.push(trace_all);
+    }
+  }
 
 
-var trace1 = {
-  type: "scatter",
-  mode: "lines",
-  name: 'AAPL High',
-  x: unpack(rows, 'Date'),
-  y: unpack(rows, 'AAPL.High'),
-  line: {color: '#17BECF'}
-}
+  var data = data_final;
+  //var data = [trace_all];
 
-var trace2 = {
-  type: "scatter",
-  mode: "lines",
-  name: 'AAPL Low',
-  x: unpack(rows, 'Date'),
-  y: unpack(rows, 'AAPL.Low'),
-  line: {color: '#7F7F7F'}
-}
+  var layout = {
+    title: 'Basic Time Series',
+    xaxis: {
+      //   tickformat: "%H:%M" // For more formatting types, see: https://github.com/d3/d3-format/blob/master/README.md#locale_format
+      title: 'Date'
+    }
+  };
 
-var data = [trace1,trace2];
-
-var layout = {
-  title: 'Basic Time Series',
-};
-
-Plotly.newPlot('myDiv', data, layout);
+  Plotly.newPlot('myDiv', data, layout);
 })
